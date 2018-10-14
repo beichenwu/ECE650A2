c952a00761925909f901cc1f777fd6ddcc30aa9e
David Wu <beichenwu4667@gmail.com>
2018-10-14 00:20:46 -0700

    Doctest added

--- a2ece650.cpp
+++ a2ece650.cpp
@@ -4,8 +4,11 @@
 #include <queue>
 #include <sstream>
 #include <cstdlib>
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "doctest-1.2.9/doctest/doctest.h"
 
 using namespace std;
+
 
 vector<vector<int> > Create_Node_list(int NumberofVertices, vector<vector<int> >& Edge_List){  //Define an array to store all node information
     vector<vector<int> > Vertex_Vector;
@@ -21,6 +24,18 @@ vector<vector<int> > Create_Node_list(int NumberofVertices, vector<vector<int> >
     return  Vertex_Vector;
 }
 
+TEST_CASE("Create_Node_list"){
+    vector<vector<int> > Edge_List;
+    vector<vector<int> > fun_output = Create_Node_list(5, Edge_List);
+    CHECK(fun_output.size()==5);
+    for(int i = 0; i< 5; i++){
+                CHECK(fun_output[i][0]==i);
+                CHECK(fun_output[i][1]==-1);
+                CHECK(fun_output[i][2]==0);
+    }
+
+}
+
 int Read_Number_Of_Vertices(string User_Input){                //Define an function to analyze the user input and return number of vertices
     int Number_Of_vertices;                                    //Create a variable to store the number of vertices
     int String_Index;                                          //a variable to find where the space from the input
@@ -30,6 +45,12 @@ int Read_Number_Of_Vertices(string User_Input){                //Define an funct
     istringstream(User_Input.substr(String_Index+1, String_Length-String_Index)) >> Number_Of_vertices;     //Read the string and get the number of vertex
 
     return  Number_Of_vertices;
+}
+
+TEST_CASE("Read_Number_Of_Vertices") {
+            CHECK(Read_Number_Of_Vertices("V 15") == 15);
+            CHECK(Read_Number_Of_Vertices("V 12") == 12);
+            CHECK(Read_Number_Of_Vertices("V 8") == 8); // will fail
 }
 
 vector<string> String_Split(string String_Input, string Deliminator){           //A Function split a string and return a vector
@@ -46,6 +67,21 @@ vector<string> String_Split(string String_Input, string Deliminator){           
     }
 
     return Splited_String;
+}
+
+TEST_CASE("String_Split") {
+    vector<string> fun_output = String_Split("<0,2>,<2,1>,<2,3>,<3,4>,<4,1>", ",");
+            CHECK(fun_output.size()==10);
+            CHECK(fun_output[0] == "<0");
+            CHECK(fun_output[1] == "2>");
+            CHECK(fun_output[2] == "<2");
+            CHECK(fun_output[3] == "1>");
+            CHECK(fun_output[4] == "<2");
+            CHECK(fun_output[5] == "3>");
+            CHECK(fun_output[6] == "<3");
+            CHECK(fun_output[7] == "4>");
+            CHECK(fun_output[8] == "<4");
+            CHECK(fun_output[9] == "1>");
 }
 
 vector<vector<int> > Read_Edge_list(string User_Input, vector<vector<int> > VerticesVector){                  //Define an function to analyze the user input and return array of edge
@@ -81,33 +117,61 @@ vector<vector<int> > Read_Edge_list(string User_Input, vector<vector<int> > Vert
     return Edge_List;
 }
 
+TEST_CASE("Read_Edge_list"){
+    vector<vector<int> > Edge_List;
+    vector<vector<int> > fun_input = Create_Node_list(10, Edge_List);
+    vector<vector<int> > fun_output = Read_Edge_list("E {<0,2>,<2,1>,<2,3>,<3,4>,<4,1>}", fun_input);
+    CHECK(fun_output.size()== 5);
+    CHECK(fun_output[0][0] == 0);
+    CHECK(fun_output[0][1] == 2);
+    CHECK(fun_output[1][0] == 2);
+    CHECK(fun_output[1][1] == 1);
+    CHECK(fun_output[2][0] == 2);
+    CHECK(fun_output[2][1] == 3);
+    CHECK(fun_output[3][0] == 3);
+    CHECK(fun_output[3][1] == 4);
+    CHECK(fun_output[4][0] == 4);
+    CHECK(fun_output[4][1] == 1);
+}
+
 vector<int> Find_Start_End(string User_Input){                                                             //Function to find Start End
     vector<int> Start_End_Vector;
     vector<string> Tmp_Edge_List = String_Split(User_Input, " ");                                          //Split String by ","
     int tmp_int;
     istringstream(Tmp_Edge_List[1]) >> tmp_int;
-    Start_End_Vector.push_back(tmp_int);                                                    //Push the first item
+    Start_End_Vector.push_back(tmp_int);                                                                  //Push the first item
     istringstream(Tmp_Edge_List[2]) >> tmp_int;
-    Start_End_Vector.push_back(tmp_int);                                                    //Push the second item
+    Start_End_Vector.push_back(tmp_int);                                                                  //Push the second item
     return  Start_End_Vector;
 }
 
+TEST_CASE("Find_Start_End"){
+    vector<int> fun_output1 = Find_Start_End("s 10 2");
+    CHECK(fun_output1.size()== 2);
+    CHECK(fun_output1[0] == 10);
+    CHECK(fun_output1[1] == 2);
+    vector<int> fun_output2 = Find_Start_End("s 2 8");
+    CHECK(fun_output2.size()== 2);
+    CHECK(fun_output2[0] == 2);
+    CHECK(fun_output2[1] == 8);
+}
+
 vector<int> Find_Connected_Vertex(int Input_Vertex, vector<vector<int> >& VerticesVector, vector<vector<int> > Edge_List){      //A Function to find the connected Vertex
-    int Orgin = Input_Vertex;                                                                                                 //Assign the Input
-    int Edge_list_Length = Edge_List.size();                                                                                  //Assign the length of Edge_list
-    vector<int> Connected_Vertex;                                                                                             //Define the vector list for Connected Vertex
-    VerticesVector = VerticesVector;                                                                                          //Assign the VerticesVector
-    for(int i = 0; i < Edge_list_Length; i++) {                                                                                //Bellman-Ford Search
+    int Orgin = Input_Vertex;                                                                                                   //Assign the Input
+    int Edge_list_Length = Edge_List.size();                                                                                    //Assign the length of Edge_list
+    vector<int> Connected_Vertex;                                                                                               //Define the vector list for Connected Vertex
+    VerticesVector = VerticesVector;                                                                                            //Assign the VerticesVector
+    for(int i = 0; i < Edge_list_Length; i++) {                                                                                 //Bellman-Ford Search
         if (Orgin == Edge_List[i][0]) {                                                                                         //Check if input vertex match
-            int Tmp_Connected_Vertex = Edge_List[i][1];                                                                       //Assign Connected the vertex
-            if (VerticesVector[Tmp_Connected_Vertex][2] != 1) {                                                               //Check if the vertex has been searched or not
-                Connected_Vertex.push_back(Tmp_Connected_Vertex);                                                             //Push to the Vector
-                VerticesVector[Tmp_Connected_Vertex][1] = Input_Vertex;                                                       //Set Parent
-                VerticesVector[Tmp_Connected_Vertex][2] = 1;                                                                 //Searched
+            int Tmp_Connected_Vertex = Edge_List[i][1];                                                                         //Assign Connected the vertex
+            if (VerticesVector[Tmp_Connected_Vertex][2] != 1) {                                                                 //Check if the vertex has been searched or not
+                Connected_Vertex.push_back(Tmp_Connected_Vertex);                                                               //Push to the Vector
+                VerticesVector[Tmp_Connected_Vertex][1] = Input_Vertex;                                                         //Set Parent
+                VerticesVector[Tmp_Connected_Vertex][2] = 1;                                                                    //Searched
             }
         }
-        if (Orgin == Edge_List[i][1]) {                                                                            //Check if input vertex match
-            int Tmp_Connected_Vertex = Edge_List[i][0];                                                                       //Same
+        if (Orgin == Edge_List[i][1]) {                                                                                         //Check if input vertex match
+            int Tmp_Connected_Vertex = Edge_List[i][0];                                                                         //Same
             if (VerticesVector[Tmp_Connected_Vertex][2] != 1) {
                 Connected_Vertex.push_back(Tmp_Connected_Vertex);
                 VerticesVector[Tmp_Connected_Vertex][1] = Input_Vertex;
@@ -117,7 +181,6 @@ vector<int> Find_Connected_Vertex(int Input_Vertex, vector<vector<int> >& Vertic
     }
     VerticesVector[Orgin][2] = 1;
     return Connected_Vertex;
-
 }
 
 vector<int> Shortest_Path(int Start, int End, vector<vector<int> >& VerticesVector, vector<vector<int> > Edge_List ){
@@ -152,7 +215,6 @@ vector<int> Shortest_Path(int Start, int End, vector<vector<int> >& VerticesVect
         Parent_Name = VerticesVector[Parent_Name][1];
     }
         return Short_Path;
-
 }
 
 string Return_Short_Path(vector<int> Short_Path){
@@ -175,18 +237,42 @@ string Return_Short_Path(vector<int> Short_Path){
     return Shortest_Path;
 }
 
+TEST_CASE("Return Short Path"){
+    vector<vector<int> > edge_list;
+    vector<vector<int> > Vertice_list = Create_Node_list(5, edge_list);
+    edge_list = Read_Edge_list("E {<0,2>,<2,1>,<2,3>,<3,4>,<4,1>}", Vertice_list);
+    vector<int> Short_path = Shortest_Path(2, 4, Vertice_list, edge_list);
+    string fun_out = Return_Short_Path(Short_path);
+    CHECK(fun_out == "2-1-4");
+    Short_path = Shortest_Path(0, 4, Vertice_list, edge_list);
+    fun_out = Return_Short_Path(Short_path);
+    CHECK(fun_out == "0-2-1-4");
+    Vertice_list = Create_Node_list(15, edge_list);
+    edge_list = Read_Edge_list("E {<2,6>,<2,8>,<2,5>,<6,5>,<5,8>,<6,10>,<10,8>,<1,7>,<5,1>,<1,2>,<3,5>,<9,6>,<1,6>,<4,2>,<7,4>,<0,1>,<0,6>}", Vertice_list);
+    Short_path = Shortest_Path(2, 7, Vertice_list, edge_list);
+    fun_out = Return_Short_Path(Short_path);
+    CHECK(fun_out == "2-1-7");
+    Short_path = Shortest_Path(1, 9, Vertice_list, edge_list);
+    fun_out = Return_Short_Path(Short_path);
+    CHECK(fun_out == "1-6-9");
+}
 
+int main(int argc, char** argv) {
 
-int main() {
+    doctest::Context ctx;
+    ctx.setOption("abort-after", 5);  // default - stop after 5 failed asserts
+    ctx.applyCommandLine(argc, argv); // apply command line - argc / argv
+    ctx.setOption("no-breaks", true); // override - don't break in the debugger
+   // ctx.setOption("no-run", true); // override - don't break in the debugger
+    int res = ctx.run();              // run test cases unless with --no-run
+    if(ctx.shouldExit())              // query flags (and --exit) rely on this
+        return res;                   // propagate the result of the tests
+
     string User_Input;                                             //Assign an empty string to read user input
     vector<vector<int> > VerticesVector;                           //Assign an pointer refer to vertices array
     vector<vector<int> > Edge_List;                                //Assign an empty vector to store the edge list
-
-
     while(true){                                                  //Main loop of the file
-
         getline(cin, User_Input);                                 //Take system standard input
-
         if(User_Input == ""){                                      //Exit the program if user input empty string
             break;
         }
@@ -209,18 +295,13 @@ int main() {
                 string Path_Output = Return_Short_Path(Path);
                 cout<<Path_Output<<endl;
             }
-
         }
         else {                                             //This should return an error to user for wrong command
-
         }
-
     }
 
     cout << "Finish reading input" << endl;
+    //return res;
     return 0;
     exit (EXIT_FAILURE);
-
-
-
 }
---
+++ cmake-build-debug/CMakeFiles/Assignment2.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wu/Desktop/Waterloo/ECE650/Assignment2/a2ece650.cpp" "/home/wu/Desktop/Waterloo/ECE650/Assignment2/cmake-build-debug/CMakeFiles/Assignment2.dir/a2ece650.cpp.o"
+  "/home/wu/Desktop/Waterloo/ECE650/Assignment2/test.cpp" "/home/wu/Desktop/Waterloo/ECE650/Assignment2/cmake-build-debug/CMakeFiles/Assignment2.dir/test.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
